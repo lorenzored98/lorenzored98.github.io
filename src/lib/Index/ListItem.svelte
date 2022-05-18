f3eb7dd381253129fed8b91c9857871e367f9c3a
lorenzored98 <lorenzopepedev@gmail.com>
2022-05-18 19:36:48 +0200

    init

---
+++ src/lib/Index/ListItem.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let href;
+	export let timestamp;
+
+	const d = new Date(timestamp * 1000);
+
+	const date = Intl.DateTimeFormat(undefined, {
+		month: "long",
+		day: "numeric",
+		year: "numeric",
+	}).format(d);
+
+	// YYYY-MM-DD https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time
+	// Surprised to not see date.toDateString() as a valid option
+	const datetime = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+</script>
+
+<li>
+	<h2><a {href} rel="nofollow noreferral noopener"><slot /></a></h2>
+	<time {datetime}>{date}</time>
+</li>
+
+<style>
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import IndexListItem from "$lib/Index/ListItem.svelte";
+</script>
+
+<h1>Lorenzo Pepe</h1>
+
+<p>Freelance front-end developer</p>
+
+<div class="socials">
+	<a
+		href="https://twitter.com/lorenzopepe98"
+		rel="noopener noreferral nofollow"
+		target="_blank"
+	>
+		Twitter
+	</a>
+	<a
+		href="https://github.com/lorenzored98"
+		rel="noopener noreferral nofollow"
+		target="_blank"
+	>
+		Github
+	</a>
+	<a
+		href="https://codesandbox.io/u/lorenzored98"
+		rel="noopener noreferral nofollow"
+		target="_blank"
+	>
+		Code
+	</a>
+</div>
+
+<div class="posts">
+	<ol>
+		<IndexListItem href="/poisson-disk-sampling" timestamp={1652888556}>
+			Poisson Disk Sampling
+		</IndexListItem>
+	</ol>
+</div>
+
+<style></style>
