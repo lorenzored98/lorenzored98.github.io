ef66ce02c2e24508bd48f00f11e449add3876cde
lorenzored98 <lorenzopepedev@gmail.com>
2022-05-23 18:52:07 +0200

    init

---
+++ src/lib/Shared/DemoContainer.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let caption;
+</script>
+
+<figure class="demo-container">
+	<figcaption>{caption}</figcaption>
+	<slot />
+</figure>
+
+<style>
+	figure {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
