14998776c787757f537dfe5962e6624ad7a7e51c
lorenzored98 <lorenzopepedev@gmail.com>
2022-05-28 11:57:43 +0200

    content not value

--- src/lib/Posts/Seo.svelte
+++ src/lib/Posts/Seo.svelte
@@ -6,7 +6,7 @@
 
 <svelte:head>
 	<title>{title}</title>
-	<meta name="description" value={description} />
+	<meta name="description" content={description} />
 	<meta name="keywords" content={keywords} />
 	<meta name="author" content="Pepe Lorenzo" />
 </svelte:head>
