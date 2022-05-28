829bb92f3847cbef4ba4f034736a831371dbc34f
lorenzored98 <lorenzopepedev@gmail.com>
2022-05-28 12:59:42 +0200

    defaults

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,16 @@ import process from "./processCodeComponent.js";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown
+			pages: "build",
+			assets: "build",
+			fallback: null,
+			precompress: false,
+		}),
+
 		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
 			default: true,
 		},
 	},
