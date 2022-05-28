118bf2e9ddfb2ad3b4d0f0b3ff013d5858ab073b
lorenzored98 <lorenzopepedev@gmail.com>
2022-05-28 14:09:07 +0200

    updated repo name change

--- src/routes/poisson-disk-sampling.svelte
+++ src/routes/poisson-disk-sampling.svelte
@@ -242,7 +242,7 @@ for (let i = 0; i < 25; i++) {
 		<p>
 			The full code for my implementation of Poisson Disk Sampling is
 			available <BlankLink
-				href="https://github.com/lorenzored98/lorenzored98.github.io/blob/main/src/lib/PoissonDiskSampling/PoissonDiskSampling.svelte#L27"
+				href="https://github.com/lorenzored98/Blog/blob/main/src/lib/PoissonDiskSampling/PoissonDiskSampling.svelte#L27"
 				>here</BlankLink
 			>.
 		</p>
