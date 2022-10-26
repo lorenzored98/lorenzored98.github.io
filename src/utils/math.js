2d8892809db674fe12a2f83641253d4180ae04d2
lorenzored98 <lorenzopepedev@gmail.com>
2022-10-26 18:03:27 +0200

    math

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,6 +1,14 @@
 // Inclusive min,max
-export const randomIntInRange = (min, max) => {
+export function randomIntInRange(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1) + min);
-};
+}
+
+export function lerp(a, b, t) {
+	return a * (1 - t) + b * t;
+}
+
+export function mapRange(inMin, inMax, outMin, outMax, t) {
+	return ((t - inMin) / (inMax - inMin)) * (outMax - outMin) + outMin;
+}
