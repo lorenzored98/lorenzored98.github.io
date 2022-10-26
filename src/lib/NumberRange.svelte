f64ffd9a0ad3cf9460dffb8806526a66a7fcc69f
lorenzored98 <lorenzopepedev@gmail.com>
2022-10-26 19:44:36 +0200

    number range

---
+++ src/lib/NumberRange.svelte
@@ -0,0 +1,105 @@
+<script>
+	import { lerp, mapRange } from "../utils/math";
+
+	export let min = 0;
+	export let max = 0;
+	export let value = 0;
+	export let step = 1;
+	export let id;
+	export let onChange;
+
+	let style;
+
+	// TODO: Lots of hardcoded values
+	$: style = `transform: translate(-50%, -50%) scale(${
+		lerp(200 - 18.5, 0, mapRange(min, max, 0, max, value) / max) / 200
+	}, 1.01)`;
+</script>
+
+<div class="wrapper">
+	<div class="input-wrapper">
+		<input
+			type="range"
+			on:input={onChange}
+			bind:value
+			{id}
+			{min}
+			{max}
+			{step}
+		/>
+		<div class="gray-bg" {style} />
+	</div>
+	<div class="ranges">
+		<span>{min}</span>
+		<span>{max}</span>
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.input-wrapper {
+		display: flex;
+	}
+
+	.gray-bg {
+		background-color: var(--disabled-bg);
+		height: 1rem;
+		width: calc(200px - 0.4rem);
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform-origin: right center;
+		pointer-events: none;
+	}
+
+	.ranges {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 0.2rem;
+	}
+
+	input[type="range"] {
+		-webkit-appearance: none;
+		appearance: none;
+		background: transparent;
+		cursor: pointer;
+		width: 200px;
+		border: none;
+		padding: 0 0.2rem;
+	}
+
+	input[type="range"]::-webkit-slider-runnable-track {
+		background: var(--bg-accent);
+		height: 1rem;
+	}
+
+	input[type="range"]::-moz-range-track {
+		background: var(--bg-accent);
+		height: 1rem;
+	}
+
+	input[type="range"]::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		appearance: none;
+		height: 1.8rem;
+		width: 1.8rem;
+		border: var(--border);
+		border-radius: 0;
+		background: white;
+		transform: translate(0, -4px);
+	}
+
+	input[type="range"]::-moz-range-thumb {
+		height: 1.6rem;
+		width: 1.6rem;
+		border: var(--border);
+		border-radius: 0;
+		background: white;
+	}
+</style>
