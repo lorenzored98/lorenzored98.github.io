f45262de00286cfdabbb49933663fb1babac24cc
lorenzored98 <lorenzopepedev@gmail.com>
2022-10-27 17:37:13 +0200

    math

--- src/utils/math.js
+++ src/utils/math.js
@@ -12,3 +12,7 @@ export function lerp(a, b, t) {
 export function mapRange(inMin, inMax, outMin, outMax, t) {
 	return ((t - inMin) / (inMax - inMin)) * (outMax - outMin) + outMin;
 }
+
+export function gammaInv(x) {
+	return x >= 0.04045 ? Math.pow((x + 0.055) / (1 + 0.055), 2.4) : x / 12.92;
+}
