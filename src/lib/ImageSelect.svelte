3df903bff999562f60c782c17c938ea309093c1e
lorenzored98 <lorenzopepedev@gmail.com>
2022-10-27 17:38:13 +0200

    image select component

---
+++ src/lib/ImageSelect.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let id;
+	export let value;
+	export let onChange;
+</script>
+
+<label for={id}>Source</label>
+<select {id} bind:value on:change={onChange}>
+	<option default value={"/images/starrynight.webp"}>Starry Night</option>
+	<option value={"/images/greatwave.webp"}>Great Wave</option>
+	<option value={"/images/akira.webp"}>Akira</option>
+	<option value={"/images/dragonquest.webp"}>Dragon Quest</option>
+</select>
+
+<style>
+	select {
+		background-color: transparent;
+		border: var(--border);
+		cursor: pointer;
+		line-height: inherit;
+	}
+</style>
