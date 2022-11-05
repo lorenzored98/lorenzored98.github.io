dcbf88ed1aa92d79c6c31ee3ee5531c5197e0e42
lorenzored98 <lorenzopepedev@gmail.com>
2022-11-05 18:46:36 +0100

    post image

---
+++ src/lib/PostImage.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let width;
+	export let height;
+	export let src;
+	export let alt;
+	export let loading = "lazy";
+</script>
+
+<div>
+	<img {src} {width} {height} {alt} {loading} />
+</div>
+
+<style>
+	div {
+		width: 100%;
+		overflow-y: none;
+		overflow-x: auto;
+		margin: 1rem 0 2rem 0;
+	}
+</style>
