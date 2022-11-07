8d8f85915cbb04576db71ad99f9b4c6af3269bc9
lorenzored98 <lorenzopepedev@gmail.com>
2022-11-07 17:04:59 +0100

    some meta tags

--- src/lib/Seo.svelte
+++ src/lib/Seo.svelte
@@ -9,4 +9,9 @@
 	<meta name="description" content={description} />
 	<meta name="keywords" content={keywords} />
 	<meta name="author" content="Pepe Lorenzo" />
+
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="article" />
 </svelte:head>
