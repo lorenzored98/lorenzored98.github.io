0ca5103e8eed0c1aaff36dc4cec1bcbf2c9cdfd2
red98 <lorenzopepedev@gmail.com>
2023-05-12 18:15:43 +0200

    style

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -1,48 +1,50 @@
 <script>
-	export let timestamp;
-	import { timestampToDate } from "$utils/date";
+  export let timestamp;
+  import { timestampToDate } from "$utils/date";
 
-	const { date, datetime } = timestampToDate(timestamp);
+  const { date, datetime } = timestampToDate(timestamp);
 </script>
 
 <div class="container">
-	<div class="not_the_title">
-		<a href="/">Back</a>
-		<time {datetime}>{date}</time>
-	</div>
-	<h1><slot /></h1>
+  <div class="not_the_title">
+    <a href="/">Back</a>
+    <time {datetime}>{date}</time>
+  </div>
+  <h1><slot /></h1>
 </div>
 
 <style>
-	.container {
-		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-		margin-bottom: 2rem;
-		width: 100%;
-	}
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-bottom: 2rem;
+    width: 100%;
+  }
 
-	.not_the_title {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		width: 100%;
-		margin-bottom: 2rem;
-	}
+  .not_the_title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 2rem;
+    border-bottom: var(--border);
+    padding: 0 0 2rem 0;
+  }
 
-	a {
-		text-decoration: underline;
-		color: var(--link);
-		margin-right: 1rem;
-	}
+  a {
+    text-decoration: underline;
+    color: var(--link);
+    margin-right: 1rem;
+  }
 
-	a,
-	time {
-		font-size: 1.6rem;
-	}
+  a,
+  time {
+    font-size: 1.6rem;
+  }
 
-	a:hover,
-	a:focus {
-		color: var(--link-highlight);
-	}
+  a:hover,
+  a:focus {
+    color: var(--link-highlight);
+  }
 </style>
